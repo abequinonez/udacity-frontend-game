ee6e227d6dd6a0c951df695b7dff8ca679eeabd4
Abraham Quinonez <abequinonez@users.noreply.github.com>
2017-07-14 02:42:18 -0700

    Removed gem1 object references

--- js/engine.js
+++ js/engine.js
@@ -98,7 +98,6 @@ var Engine = (function(global) {
             enemy.update(dt);
         });
         player.update();
-        // gem1.update();
     }
 
     /* This function initially draws the "game level", it will then call
@@ -151,16 +150,13 @@ var Engine = (function(global) {
         /* Loop through all of the objects within the allEnemies array and call
          * the render function you have defined.
          */
-        // gem1.render();
         gems.forEach(function(gem) {
             gem.render();
         });
         allEnemies.forEach(function(enemy) {
             enemy.render();
         });
-
         player.render();
-        
     }
 
     /* This function does nothing but it could have been a good place to
